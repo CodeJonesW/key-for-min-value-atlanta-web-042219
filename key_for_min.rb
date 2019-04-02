9ac1b826bec816132ee4303f8266341fe116aff0
Will Jones <willjones2010@gmail.com>
2019-04-02 02:40:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
-  if hash = {}
+  if hash == {}
     return nil
  else  
   low_value = 10000
